8c594f3ef6bc3e0ad0519c1276374c175bfb178f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-20 00:14:39 +0300

    validate username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true, uniqueness: true
   has_many :tasks, dependent: :destroy
 end
