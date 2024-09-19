344cb5fadc728419c134d92107a3f84d61836918
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-20 01:17:12 +0300

    add association with category

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  belongs_to :category
   validates :title, presence: true
   scope :incomplete, -> { where(completed: false) }
   scope :completed, -> { where(completed: true) }
