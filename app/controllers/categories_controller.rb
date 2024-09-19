c7cf0228f2d487b53bf7128427e113d38bf3d27f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-20 01:25:28 +0300

    add category controller methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,50 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = current_user.categories
+  end
+
+  def show
+  end
+
+  def new
+    @category = current_user.categories.build
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      redirect_to dashboard_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to dashboard_path, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to dashboard_path, notice: 'Category was successfully deleted.'
+  end
+
+  private
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :color)
+  end
 end
