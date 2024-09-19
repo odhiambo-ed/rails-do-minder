adb74f10203a8187e88928537b6a39c66987b39c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-09-20 00:07:43 +0300

    validate task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  scope :incomplete, -> { where(completed: false) }
+  scope :completed, -> { where(completed: true) }
 end
